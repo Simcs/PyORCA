8adc542caa3ebca4c54a8b807c5d0afd1b08b97a
Simcs <smh4477@naver.com>
2021-05-24 20:10:56 +0900

    minor modification

--- simulator.py
+++ simulator.py
@@ -1,6 +1,6 @@
 import numpy as np
 from agent import Agent
-from kd_tree import KDTree
+
 
 class RVOSimulator(object):
 
@@ -10,20 +10,7 @@ class RVOSimulator(object):
         self.time_step = 0.0
         self.default_agent = None
         self.agents = []
-        self.kd_tree = KDTree(self)
 
-    # def __init__(self, time_step, time_horizon, radius, max_speed, velocity):
-        
-    #     self.global_time = 0.0
-    #     self.time_step = time_step
-    #     self.agents = []
-
-    #     self.default_agent = Agent(self)
-    #     self.default_agent.time_horizon = time_horizon
-    #     self.default_agent.radius = radius
-    #     self.default_agent.max_speed = max_speed
-    #     self.default_agent.velocity = velocity
-    
     def addAgent(self, position):
         agent = Agent(self)
         agent.position = position
@@ -52,20 +39,6 @@ class RVOSimulator(object):
         self.agents.append(agent)
         return len(self.agents) - 1
 
-        
-    
-    # def addAgent(sefl, position, time_horizon, radius, max_speed, velocity):
-    #     agent = Agent(self)
-    #     agent.position = position
-    #     agnet.max_speed = max_speed
-    #     agent.radius = radius
-    #     agent.time_horizon = time_horizon
-    #     agent.velocity = velocity
-    #     agent.id = len(self.agents)
-
-    #     self.agents.append(agent)
-    #     return len(self.agents) - 1
-
     def computeAgentNeighbors(self, agent):
         for a in self.agents:
             if a == agent:
@@ -73,8 +46,6 @@ class RVOSimulator(object):
             agent.agent_neighbors.append(a)
     
     def doStep(self):
-
-        # self.kd_tree.buildAgentTree()
 
         for agent in self.agents:
             if agent.is_obstacle:
@@ -98,14 +69,3 @@ class RVOSimulator(object):
         self.default_agent.radius = radius
         self.default_agent.max_speed = max_speed
         self.default_agent.velocity = velocity
-
-    # def setAgentDefaults(self, neighbor_dist, max_neighbors, time_horizon, radius, max_speed, velocity):
-    #     if self.default_agent is None:
-    #         self.default_agent = Agent(self)
-        
-    #     self.default_agent.neighbor_dist = neighbor_dist
-    #     self.default_agent.max_neighbors = max_neighbors
-    #     self.default_agent.time_horizon = time_horizon
-    #     self.default_agent.radius = radius
-    #     self.default_agent.max_speed = max_speed
-    #     self.default_agent.velocity = velocity
